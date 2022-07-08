60c04b23a4a1078fed896739e32df1a5c9158eb3
Roman Voronin <zeromistakes4@gmail.com>
2022-07-08 20:20:01 +0300

    added random char logic

--- src/index.js
+++ src/index.js
@@ -2,12 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/App';
 import './style/style.scss';
-import MarvelService from "./services/MarvelService";
-import {logDOM} from "@testing-library/react";
-
-const marvelService = new MarvelService();
-
-marvelService.getCharacter(1010338).then(res => console.log(res));
 
 ReactDOM.render(
   <React.StrictMode>
