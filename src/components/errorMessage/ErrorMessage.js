2de0fe7a48230c6c9243dc2e0c87bf0af424d32d
Roman Voronin <zeromistakes4@gmail.com>
2022-07-09 09:19:05 +0300

    added spinner and error component

---
+++ src/components/errorMessage/ErrorMessage.js
@@ -0,0 +1,10 @@
+import error from './error.gif';
+const ErrorMessage = () => {
+    return (
+        <img src={error}
+             alt="error"
+             style={{ display: 'block', width: "250px", height: "250px",objectFit: 'contain', margin: "0 auto"}}/>
+    )
+}
+
+export default ErrorMessage;
