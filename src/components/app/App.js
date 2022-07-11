c019aabe81fbff4eeef3b7e37901dfed4bbebc73
Roman Voronin <zeromistakes4@gmail.com>
2022-07-11 18:15:28 +0300

    added CharList / ErrorBoundary components

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,24 +1,41 @@
+import {Component} from "react";
 import AppHeader from "../appHeader/AppHeader";
 import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
 import CharInfo from "../charInfo/CharInfo";
-
+import ErrorBoundary from "../errorBoundary/ErrorBoundary";
 import decoration from '../../resources/img/vision.png';
 
-const App = () => {
-    return (
-        <div className="app">
-            <AppHeader/>
-            <main>
-                <RandomChar/>
-                <div className="char__content">
-                    <CharList/>
-                    <CharInfo/>
-                </div>
-                <img className="bg-decoration" src={decoration} alt="vision"/>
-            </main>
-        </div>
-    )
+class App extends Component {
+    state = {
+        selectedChar: null,
+    }
+
+    setSelectedChar = (id) => {
+        this.setState({
+            selectedChar: id
+        })
+    }
+    render() {
+        return (
+            <div className="app">
+                <AppHeader/>
+                <main>
+                    <ErrorBoundary>
+                        <RandomChar/>
+                    </ErrorBoundary>
+                    <div className="char__content">
+                        <ErrorBoundary>
+                            <CharList onCharSelected={this.setSelectedChar}/>
+                            <CharInfo charId={this.state.selectedChar}/>
+                        </ErrorBoundary>
+                    </div>
+                    <img className="bg-decoration" src={decoration} alt="vision"/>
+                </main>
+            </div>
+        )
+    }
+
 }
 
 export default App;
--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -18,6 +18,11 @@ class RandomChar extends Component {
     }
     marvelService = new MarvelService();
 
+    onCharLoading = () => {
+        this.setState({
+            loading: true,
+        })
+    }
     onCharLoaded = (char) => {
         this.setState({char, loading: false})
     }
@@ -28,6 +33,7 @@ class RandomChar extends Component {
 
     updateChar = () => {
         const id = Math.round(Math.random() * (1011400 - 1011000) + 1011000);
+        this.onCharLoading();
         this.marvelService.getCharacter(id)
             .then(this.onCharLoaded)
             .catch(this.onError);
